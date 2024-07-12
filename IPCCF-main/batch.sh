4147e5aaa0c883b7c0e7ec0d6ce4ccffa1363c07
rookitkitlee <1010803363@qq.com>
2024-07-12 19:57:47 +1000

    init

---
+++ IPCCF-main/batch.sh
@@ -0,0 +1,4 @@
+python main.py --epoch 500 --dataset gowalla --n_intents 8 --intent_normalize True --ssl_reg 0.08
+python main.py --epoch 200 --dataset amazon --n_intents 8
+python main.py --epoch 200 --dataset tmall --n_intents 8
+
